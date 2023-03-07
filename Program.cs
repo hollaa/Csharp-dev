3abff7471413d816ac260dc44ff0c5e5b575cdde
Alica Holla <alica.holla@gmail.com>
2023-03-07 14:01:09 +0100

    missing checking for sort inpt

--- Program.cs
+++ Program.cs
@@ -1,33 +1,46 @@
 ﻿using System;
 
-class FirstAssignment {
-    static void Main(string[] args) {
+class FirstAssignment
+{
+    static void Main(string[] args)
+    {
+
+        
+        if (args[0] != "sort")
+        {
+            Console.WriteLine("Input not correct. Check the documentation to be inspired for the input format");
+            return;
+        }
 
         //to call the bubble sort function, the input should be -Bubble
-        if (args[0] == "-Bubble") {
-            int[] arr = ParseInput(args[1]);
+        if (args[1] == "-Bubble")
+        {
+            int[] arr = ParseInput(args[2]);
             BubbleSort(arr); //calling the bubble sort, passing the numbers the user gave us
             PrintArray(arr);
         }
         //to call the merge sort function, the input should be -Merge
-        else if (args[0] == "-Merge") {
-            int[] arr = ParseInput(args[1]);
+        else if (args[1] == "-Merge")
+        {
+            int[] arr = ParseInput(args[2]);
             MergeSort(arr, 0, arr.Length - 1);
             PrintArray(arr);
         }
-
-        else {
+        else
+        {
             Console.WriteLine("Please specify the sort you want. Can be either -Bubble or -Merge.");
         }
-        
+
     }
 
     //parsing the string input into the list of numbers
-    static int[] ParseInput(string input) {
+    static int[] ParseInput(string input)
+    {
         string[] numbers = input.Split(',');
         int[] arr = new int[numbers.Length];
 
-        for (int i = 0; i < numbers.Length; i++) {
+        for (int i = 0; i < numbers.Length; i++)
+        {
             arr[i] = int.Parse(numbers[i]);
         }
 
@@ -35,9 +48,11 @@ class FirstAssignment {
     }
 
     //function to print the array after sorting, passing the array 
-    static void PrintArray(int[] arr) {
+    static void PrintArray(int[] arr)
+    {
         Console.WriteLine("This is your sorted array: ");
-        for (int i = 0; i < arr.Length; i++) {
+        for (int i = 0; i < arr.Length; i++)
+        {
             Console.Write(arr[i] + " ");
         }
         Console.WriteLine();
@@ -46,19 +61,23 @@ class FirstAssignment {
 
 
     //Bubble sort brings the biggest number to the end by comparing each number at each index, if its greater, they swap
-    static void BubbleSort(int[] unsortedArray) {
+    static void BubbleSort(int[] unsortedArray)
+    {
         int length = unsortedArray.Length;
         int temp;
         int position;
         bool swapped = true; //need to check if the number has been swapped
 
         //the loop goes to the length of the array - 1 because we are comparing to the number after the current number -> no need to go to the last position as there is no number after it
-        for (int i = 0; i < length - 1; i++) {
+        for (int i = 0; i < length - 1; i++)
+        {
             swapped = false;
-            position = length - i -1; //adding i to not go to the last element as that has already been moved to the right
+            position = length - i - 1; //adding i to not go to the last element as that has already been moved to the right
 
-            for (int j = 0; j < position; j++) {
-                if (unsortedArray[j] > unsortedArray[j + 1]) { //if current element is greater than the next one, swap it forward
+            for (int j = 0; j < position; j++)
+            {
+                if (unsortedArray[j] > unsortedArray[j + 1])
+                { //if current element is greater than the next one, swap it forward
 
                     temp = unsortedArray[j];
                     unsortedArray[j] = unsortedArray[j + 1];
@@ -68,7 +87,8 @@ class FirstAssignment {
             }
 
             //if no swap was made during the iteration, the array is already sorted
-            if (swapped == false) {
+            if (swapped == false)
+            {
                 break;
             }
         }
@@ -76,9 +96,11 @@ class FirstAssignment {
 
     //the algorithm splits the arrays until we got two arrays of one element each
     //recursively sort the left and right halves of the array
-    static void MergeSort(int[] arr, int left, int right) {
+    static void MergeSort(int[] arr, int left, int right)
+    {
         //when the function is called for the first time, left = 0 and right = the length of the array - 1, so in the beginning, the array is always divided into two parts
-        if (left < right) {
+        if (left < right)
+        {
             int mid = (left + right) / 2; //calculating the middle index of the array
 
             MergeSort(arr, left, mid); //recursively sort the left half of the array
@@ -89,7 +111,8 @@ class FirstAssignment {
     }
 
     //Merge the two sorted halves into a single sorted array
-    static void Merge(int[] arr, int left, int mid, int right) {
+    static void Merge(int[] arr, int left, int mid, int right)
+    {
         int i, j, k;
         int sizeLeft = mid - left + 1; //the size of the left subarray
         int sizeRight = right - mid; // the size of the right subarray
@@ -97,25 +120,30 @@ class FirstAssignment {
         int[] tempLeft = new int[sizeLeft]; //temporary array for the left subarray
         int[] temptRight = new int[sizeRight]; //temporary array for the right subarray
 
-        for (i = 0; i < sizeLeft; i++) { //copying the left subarray to the temporary 
+        for (i = 0; i < sizeLeft; i++)
+        { //copying the left subarray to the temporary 
             tempLeft[i] = arr[left + i];
         }
 
-        for (j = 0; j < sizeRight; j++) { //copying the right subarray to the temporary 
+        for (j = 0; j < sizeRight; j++)
+        { //copying the right subarray to the temporary 
             temptRight[j] = arr[mid + 1 + j];
         }
 
-        i = 0; // indec for calculating for left array
+        i = 0; // index for calculating for left array
         j = 0; //index for calculating the right array
         k = left; //the index of the initial array
 
 
-        while (i < sizeLeft && j < sizeRight) {
-            if (tempLeft[i] <= temptRight[j]) { //comparing each element on each index, 
+        while (i < sizeLeft && j < sizeRight)
+        {
+            if (tempLeft[i] <= temptRight[j])
+            { //comparing each element on each index, 
                 arr[k] = tempLeft[i]; //if element from left array is smaller, write it to the initial array
                 i++;
             }
-            else {
+            else
+            {
                 arr[k] = temptRight[j];
                 j++;
             }
@@ -123,13 +151,15 @@ class FirstAssignment {
             k++;
         }
 
-        while (i < sizeLeft) { //if there are any elements remaining in the left subarray, copy them to the initial array
+        while (i < sizeLeft)
+        { //if there are any elements remaining in the left subarray, copy them to the initial array
             arr[k] = tempLeft[i];
             i++;
             k++;
         }
 
-        while (j < sizeRight) { //if there are any elements remaining in the right subarray, copy them to the initial array
+        while (j < sizeRight)
+        { //if there are any elements remaining in the right subarray, copy them to the initial array
             arr[k] = temptRight[j];
             j++;
             k++;
